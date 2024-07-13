e2f42911cabaac670755f4182f694223b7c86826
Shevaitverma <shevaitverma333@gmail.com>
2024-07-13 20:15:33 +0530

    completed task 6

---
+++ Day1/Activity5/task6.js
@@ -0,0 +1,17 @@
+const a = 425234
+console.log(a);
+
+a = 4322
+console.log(a);
+
+/* 
+TypeError: Assignment to constant variable.
+    at Object.<anonymous> (c:\Users\sheva\OneDrive\Desktop\30_DAYS_JAVASCRIPT_CHALLENGE\Day1\Activity5\task6.js:4:3)
+    at Module._compile (node:internal/modules/cjs/loader:1358:14)
+    at Module._extensions..js (node:internal/modules/cjs/loader:1416:10)
+    at Module.load (node:internal/modules/cjs/loader:1208:32)
+    at Module._load (node:internal/modules/cjs/loader:1024:12)
+    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:174:12)
+    at node:internal/main/run_main_module:28:49
+
+*/
